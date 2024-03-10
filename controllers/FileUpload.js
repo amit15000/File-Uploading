1e6ef3eff508d9fca41c63b01d6cacc7044b5a56
Amit Kumar <15000amitkumar@gmail.com>
2024-03-10 09:15:08 +0530

    File Upload Successful

--- controllers/FileUpload.js
+++ controllers/FileUpload.js
@@ -2,17 +2,19 @@ exports.localFileUpload = async (req, res) => {
   try {
     const file = req.files.myfile;
     console.log("File =>", file);
-    let path = __dirname + "/files" + Date.now();
+    let path =
+      __dirname + "/files" + Date.now() + `.${file.name.split(".")[1]}`;
 
     file.mv(path, (err) => {
-      console.error(err);
+      if (err) {
+        console.error(err);
+      }
     });
-    res.send(
-      json({
-        success: true,
-        messsage: "File Uploaded Successfully",
-      })
-    );
+
+    res.json({
+      success: true,
+      messsage: "File Uploaded Successfully",
+    });
   } catch (err) {
     console.log("Error in uploading the file");
     console.error(err);
