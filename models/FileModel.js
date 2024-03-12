f7a21e879b9b0a1cf4e61c1b3c09c94ae2b208d4
Amit Kumar <15000amitkumar@gmail.com>
2024-03-12 18:15:56 +0530

    Main Sending

--- models/FileModel.js
+++ models/FileModel.js
@@ -1,5 +1,6 @@
 const { default: mongoose } = require("mongoose");
-
+require("dotenv").config();
+const { transporter } = require("../config/transporter");
 const fileSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -15,5 +16,28 @@ const fileSchema = new mongoose.Schema({
     type: String,
   },
 });
+//post
+
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("Saved Document", doc);
+
+    //transporter
+
+    //send mail
+    let info = await transporter.sendMail({
+      from: "EdTech",
+      to: doc.email,
+      subject: "New File Uploaded to Cloudinary",
+      html: `<h2>File Uploaded At ${Date()} </h2> <p><a href="${
+        doc.fileUrl
+      }">Click To Visit</a></p>`,
+    });
+    console.log(info);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 const File = mongoose.model("File", fileSchema);
 module.exports = File;
